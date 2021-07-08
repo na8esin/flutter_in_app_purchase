ed6b3fcbf8d11b9efc6ee717d683e8fda8282afe
渡辺孝之 <na8esin.mpst@gmail.com>
2021-07-08 11:18:24 +0900

    build関数の中でもplatformは判別できる

--- lib/platform.dart
+++ lib/platform.dart
@@ -1,4 +1,5 @@
 import 'dart:io' show Platform, stdout;
+import 'package:flutter/material.dart';
 
 void main() {
   // Get the operating system as a string.
@@ -12,5 +13,35 @@ void main() {
     print('is a Android');
   } else {
     print('something else.');
+  }
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // この判定もdebugモードで動作する
+    Widget? eachPlatform;
+    if (Platform.isIOS) {
+      eachPlatform = Text('ios');
+    } else if (Platform.isAndroid) {
+      eachPlatform = ListTile(
+        title: Text('android'),
+      );
+    } else {
+      eachPlatform = Container();
+      print('something else.');
+    }
+    return MaterialApp(
+      home: Scaffold(
+        // SafeAreaがないと上の時計の部分に文字が入り込む
+        body: SafeArea(
+          child: Container(
+            child: eachPlatform,
+          ),
+        ),
+      ),
+    );
   }
 }
